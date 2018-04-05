1cead3d190358b9c218f7a8be26adbfc16674402
ErnestFan <ernestfan1019@gmail.com>
2018-04-04 23:08:52 -0700

    Initialization done

---
+++ pixel-city/Controller/MapVC.swift
@@ -0,0 +1,25 @@
+//
+//  MapVC.swift
+//  pixel-city
+//
+//  Created by Ernest Fan on 2018-04-04.
+//  Copyright © 2018 ERF. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
